78e08e6c3b562f638040c39d155628cb405277cf
Mateo Monsalve <mateo.monsalve@utp.edu.co>
2025-09-09 00:08:30 -0500

    primera implementación del backend y distribución en carpetas

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,21 +1,22 @@
 import { DataSource } from "typeorm";
 import { User } from "../models/User";
 
-// Verifica que las variables de entorno estén cargadas
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // Asegúrate de que esto no sea undefined
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [User],
   migrations: [],
   subscribers: [],
+  ssl: true, // Habilita SSL
+  extra: {
+    ssl: {
+      rejectUnauthorized: false, // Necesario para Render.com
+    },
+  },
 });
